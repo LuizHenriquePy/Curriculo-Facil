90a6c7c976ad72c6de8aff7bc077529e4f50e5d2
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2020-07-01 15:55:22 -0300

    adicionado o arquivo GUI.py

---
+++ GUI.py
@@ -0,0 +1,51 @@
+from tkinter import *
+
+
+class Caixa_de_dados:
+
+    def __init__(self, row, frame_right, frame_left, window):
+
+        self.row = row
+        self.fr_right = frame_right
+        self.fr_left = frame_left
+        self.window = window
+        
+     def main(self):
+         pass
+
+
+
+class Primeiras_infos(Caixa_de_dados):
+    pass
+
+
+
+class Dados_pessoais(Caixa_de_dados):
+    pass
+
+
+
+class Escolaridade(Caixa_de_dados):
+    pass
+
+
+
+class Cursos(Caixa_de_dados):
+    pass
+
+
+
+class Experiencias_profissionais(Caixa_de_dados):
+    pass
+
+
+
+class Experiencias_profissionais_sem_registro(Caixa_de_dados):
+    pass
+
+
+class Menu:
+    pass
+
+class Window:
+    pass
