1ac64abd41f607e0480c4b8e1fa927cf6c2c7125
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2020-07-02 17:52:01 -0300

    Adicionado um exemplo de como seria o dicionario com os dados do usuario

--- GUI.py
+++ GUI.py
@@ -1,5 +1,90 @@
 from tkinter import *
 
+pessoa = {
+    "PRIMEIRAS INFORMAÇÕES": {
+        "TITULO": "",
+        "NOME": None,
+        "ENDEREÇO": None,
+        "TELEFONES": None,
+        "EMAILS": None
+        },
+    
+    "DADOS PESSOAIS": {
+        "TITULO": "DADOS PESSOAIS",
+        "DATA DE NASCIMENTO": None,
+        "NATURALIDADE": None,
+        "NASCIONALIDADE": None,
+        "ESTADO CIVIL": None,
+        "HABILITAÇÂO": None
+        },
+    
+    "FORMACÂO": {
+        "TITULO": "FORMACÂO",
+        "GRAU": None,
+        "STATUS": None
+        },
+    
+    "CURSOS": {
+        "TITULO": "CURSOS",
+        "NOME": None,
+        "LOCAL": None,
+        "STATUS": None
+        },
+
+    "EXPERIÊNCIAS PROFISSIONAIS": {
+        "TITULO": "EXPERIÊNCIAS PROFISSIONAIS",
+        "EMPREGO1": {
+            "EMPRESA": None,
+            "FUNÇÂO": None,
+            "PERÌODO": None
+            },
+        "EMPREGO2": {
+            "EMPRESA": None,
+            "FUNÇÂO": None,
+            "PERÌODO": None
+            },
+        "EMPREGO2": {
+            "EMPRESA": None,
+            "FUNÇÂO": None,
+            "PERÌODO": None
+            }
+        },
+    
+    "EXPERIÊNCIAS PROFISSIONAIS SEM REGISTRO": {
+        "TITULO": "EXPERIÊNCIAS PROFISSIONAIS SEM REGISTRO",
+        "EMPREGO1": {
+            "EMPRESA": None,
+            "FUNÇÂO": None,
+            "PERÌODO": None
+            },
+        "EMPREGO2": {
+            "EMPRESA": None,
+            "FUNÇÂO": None,
+            "PERÌODO": None
+            },
+        "EMPREGO2": {
+            "EMPRESA": None,
+            "FUNÇÂO": None,
+            "PERÌODO": None
+            }
+        },
+
+    "OBJETIVO": {
+        "TITULO": "OBJETIVO",
+        "TEXTO": None
+        },
+
+    "INFORMAÇÔES COMPLEMENTARES": {
+        "TITULO": "INFORMAÇÔES COMPLEMENTARES",
+        "TEXTO": None
+        },
+
+    "PERFIL":{
+        "TITULO": "INFORMAÇÔES COMPLEMENTARES",
+        "TEXTO": None
+        }
+    }
+
 
 class Caixa_de_dados:
 
@@ -10,42 +95,211 @@ class Caixa_de_dados:
         self.fr_left = frame_left
         self.window = window
         
-     def main(self):
-         pass
+    def main(self):
+        pass
+
+    def _get(self):
+        pass
+
+    def _set(self):
+        pass
 
 
 
-class Primeiras_infos(Caixa_de_dados):
-    pass
+class Primeiras_info(Caixa_de_dados):
+
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
 
 
 
 class Dados_pessoais(Caixa_de_dados):
-    pass
+
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
 
 
 
 class Escolaridade(Caixa_de_dados):
-    pass
+
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
 
 
 
 class Cursos(Caixa_de_dados):
-    pass
+
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
 
 
 
 class Experiencias_profissionais(Caixa_de_dados):
-    pass
+
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
 
 
 
 class Experiencias_profissionais_sem_registro(Caixa_de_dados):
-    pass
+
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
+
+
+
+class Objetivo(Caixa_de_dados):
+    
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
+
+
+
+class Info_complementares(caixa_de_dados):
+    
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
+
+
+
+class Perfil(Caixa_de_dados):
+    
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
+
+
+
+class Caixa_generica(Caixa_de_dados):
+    
+    def __init__(self):
+        pass
+    
+
+    def main(self):
+        pass
+
+
+    def _get(self):
+        pass
+
+
+    def _set(self):
+        pass
 
 
 class Menu:
     pass
 
 class Window:
+    pass
+
+if __name__ == '__main__':
     pass
